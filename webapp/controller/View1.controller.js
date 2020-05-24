352bcfbac70176eb96478884ccd9ad92ec52d7db
Fausto Sasdelli Junior <faustosasdelli@gmail.com>
2020-05-24 20:33:37 +0000

    teste

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,6 +7,8 @@ sap.ui.define([
 	return Controller.extend("ovly.data.binding.curriculo.controller.View1", {
 		onInit: function () {
 			
+			window._painel = this.byId("panel_competencias");
+			
 			var oFonteDeDados = {
 				nome: "Fausto",
 				sobrenome: "Sasdelli",
@@ -23,15 +25,29 @@ sap.ui.define([
 				},
 				quiz_1: 60,
 				quiz_1_media: 3,
-				competencias: [ "ABAP", "UI5", "JavaScript" ]
+				competencias: [ "ABAP", "UI5", "JavaScript" ],
+				empresas: [
+					{ nome: "Resource", cargo: "Abap Developer" },
+					{ nome: "EGM", cargo: "COBOL Developer" },
+					{ nome: "Exxon", cargo: "Technical Analist" }
+				]
 			};
 			
 			//var oModeloPessoal = new JSONModel(oFonteDeDados);
 			
-			var oModeloPessoal = new JSONModel("https://www.mocky.io/v2/5ec5da333200007900d748f3"); //aqui faz uma chamada de rede , um GET na uri
- 			
+			var oModeloPessoal = new JSONModel("https://www.mocky.io/v2/5ec86c462f0000bc1adb6fe9"); //aqui faz uma chamada de rede , um GET na uri
 			this.getView().setModel(oModeloPessoal);
 			
+/*			var sGitHub = "https://api.github.com/users/sasdelliju/repos";
+			var oModeloGitHub = new JSONModel(sGitHub);
+			this.byId("table_github").setModel(oModeloGitHub, "github");*/ //como estou passando o segundo modelo, preciso do segindo parametro pra 1 nao enconbrir o outro
+		},
+		
+		toUpper : function(sTexto){
+			if(!sTexto){
+				return "???";
+			}
+			return sTexto.toUpperCase();
 		}
 	});
 });
